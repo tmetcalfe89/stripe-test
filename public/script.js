10dfaaa4663d8f6ebd19d45c9027ed608fc488f9
tmetcalfe89 <tmetcalfe89@gmail.com>
2023-07-20 20:18:19 -0700

    abstracted pk to server

--- public/script.js
+++ public/script.js
@@ -1,49 +1,50 @@
-var stripe = Stripe(
-  "pk_test_51MTphvIQbC6nvg4PIjNNjuyxqvqKCjEjpgscDOvgVpkpdIOOltQovaup7fH7rKq4pIfS7MaHvXSWLh1waL2aONRu00vcmZKF3x"
-);
+(async function () {
+  const pk = await (await fetch("/api/stripe/pk")).text();
+  const stripe = Stripe(pk);
 
-document.getElementById("pay").addEventListener("click", async () => {
-  //gonna charge $5
+  document.getElementById("pay").addEventListener("click", async () => {
+    //gonna charge $5
 
-  // Create PaymentIntent
-  const piResponse = await fetch("/api/stripe/paymentintent", {
-    method: "POST",
-  });
-  const paymentIntent = await piResponse.json();
-
-  console.log(paymentIntent);
-
-  const options = {
-    clientSecret: paymentIntent.client_secret,
-  };
-
-  const elements = stripe.elements(options);
-  const paymentElement = elements.create("payment");
-  paymentElement.mount("#payment-element");
-
-  const form = document.getElementById("payment-form");
-
-  form.addEventListener("submit", async (event) => {
-    event.preventDefault();
-
-    const { error } = await stripe.confirmPayment({
-      //`Elements` instance that was used to create the Payment Element
-      elements,
-      confirmParams: {
-        return_url: "http://localhost:3000?id=123",
-      },
+    // Create PaymentIntent
+    const piResponse = await fetch("/api/stripe/paymentintent", {
+      method: "POST",
     });
+    const paymentIntent = await piResponse.json();
 
-    if (error) {
-      // This point will only be reached if there is an immediate error when
-      // confirming the payment. Show error to your customer (for example, payment
-      // details incomplete)
-      const messageContainer = document.querySelector("#error-message");
-      messageContainer.textContent = error.message;
-    } else {
-      // Your customer will be redirected to your `return_url`. For some payment
-      // methods like iDEAL, your customer will be redirected to an intermediate
-      // site first to authorize the payment, then redirected to the `return_url`.
-    }
+    console.log(paymentIntent);
+
+    const options = {
+      clientSecret: paymentIntent.client_secret,
+    };
+
+    const elements = stripe.elements(options);
+    const paymentElement = elements.create("payment");
+    paymentElement.mount("#payment-element");
+
+    const form = document.getElementById("payment-form");
+
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
+
+      const { error } = await stripe.confirmPayment({
+        //`Elements` instance that was used to create the Payment Element
+        elements,
+        confirmParams: {
+          return_url: "http://localhost:3000?id=123",
+        },
+      });
+
+      if (error) {
+        // This point will only be reached if there is an immediate error when
+        // confirming the payment. Show error to your customer (for example, payment
+        // details incomplete)
+        const messageContainer = document.querySelector("#error-message");
+        messageContainer.textContent = error.message;
+      } else {
+        // Your customer will be redirected to your `return_url`. For some payment
+        // methods like iDEAL, your customer will be redirected to an intermediate
+        // site first to authorize the payment, then redirected to the `return_url`.
+      }
+    });
   });
-});
+})();
--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const express = require("express");
 const path = require("path");
 const stripe = require("stripe");
 
-const { STRIPE_API_KEY } = process.env;
+const { STRIPE_API_KEY, STRIPE_PK } = process.env;
 
 const app = express();
 const stripeClient = stripe(STRIPE_API_KEY);
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/api/stripe/pk", async (req, res) => {
+  res.send(STRIPE_PK);
+});
 
 app.post("/api/stripe/paymentintent", async (req, res) => {
   const paymentIntent = await stripeClient.paymentIntents.create({
